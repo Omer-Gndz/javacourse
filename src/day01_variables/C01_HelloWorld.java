0b089343b85af2ad5ad94da7cf4ff053a2b43772
Omer-Gndz <omrgndz56@gmail.com>
2022-08-24 23:00:22 +0200

    25-08-2022

--- src/day01_variables/C01_HelloWorld.java
+++ src/day01_variables/C01_HelloWorld.java
@@ -5,7 +5,7 @@ public class C01_HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println();
 
     }
    }
---
+++ src/day999/c0111.java
@@ -0,0 +1,7 @@
+package day999;
+
+public class c0111 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
